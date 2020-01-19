de156769ffd9f42957a8c7b8cd3d993a89c0d51f
dsreliete <dsreliete@gmail.com>
2020-01-19 17:50:11 -0500

    delete comment code

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -100,29 +100,6 @@ class Reservation extends Component {
                             accessibilityLabel='Tap me to search for available campsites to reserve'
                         />
                     </View>
-                    {/* <Modal
-                        animationType={'slide'}
-                        transparent={false}
-                        visible={this.state.showModal}
-                        onRequestClose={() => this.toggleModal()}>
-                        <View style={styles.modal}>
-                            <SafeAreaView 
-                                forceInset={{top: 'always', horizontal: 'never'}}>
-                            </SafeAreaView>
-                            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                            <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
-                            <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                            <Text style={styles.modalText}>Date: {this.state.date}</Text>
-                            <Button
-                                onPress={() => {
-                                    this.toggleModal();
-                                    this.resetForm();
-                                }}
-                                color='#5637DD'
-                                title='Close'
-                            />
-                        </View>
-                    </Modal> */}
             </Animatable.View>
         );
     }
