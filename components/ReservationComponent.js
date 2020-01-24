231056c5a6c71dd9c3e20424c88b7025ea8cbde5
dsreliete <dsreliete@gmail.com>
2020-01-24 11:14:26 -0500

    added notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -26,7 +28,11 @@ class Reservation extends Component {
             `Number of Campers: ${this.state.campers}\n\nHike-In?: ${this.state.hikeIn}\n\nDate: ${this.state.date}`,
             [
                 { text: 'Cancel', onPress: () => this.resetForm() },
-                { text: 'OK', onPress: () => this.resetForm() },
+                { text: 'OK', onPress: () => {
+                    this.presentLocalNotification(this.state.date);
+                    this.resetForm();
+                    } 
+                },
             ],
             {cancelable: false},
             );
@@ -39,6 +45,28 @@ class Reservation extends Component {
             date: '',
             showModal: false
         });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     render() {
